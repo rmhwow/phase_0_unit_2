2de45791ff0730dd68eeab8b05db139587d06b2a
Morgan <rmhwow@gmail.com>
2014-05-16 18:44:08 -0500

    driver code

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -8,12 +8,19 @@
 # 1. Pseudocode
 
 # What is the input?
+#The input is any positive Numbers
 # What is the output? (i.e. What should the code return?)
+#The code should return that number with the commas in the proper place.
 # What are the steps needed to solve the problem?
+#define method called separate_comma
+#have it take the argument number
 
 
 # 2. Initial Solution
-
+def separate_comma(num)
+    if num= 1..999
+        puts num
+        elsif num<10,000
 
 
 
